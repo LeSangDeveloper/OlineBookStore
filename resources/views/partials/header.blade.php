3ba18a70928a250adc7157af80e314e1160ba172
LeeShangCyber <lesang541191309@gmail.com>
2019-10-28 18:19:38 +0700

    create header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="{{ route('product.index') }}">LS Bookstore</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#"> Link <span class="sr-only">(current)</span></a></li>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="{{ route('product.getShoppingCart') }}"><i class="fa fa-shopping-cart"></i>
+        Shopping cart <span class="badge"> {{ Session::has('cart')?Session::get('cart')->totalQty:'' }} </span>
+      </a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-users"></i> Manage User <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            @if(Auth::check())
+            <li><a href="{{ route('user.profile')}}"> Profile </a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="{{ route('user.logout') }}"> Log Out </a></li>
+            @else
+            <li><a href="{{ route('user.signup') }}"> Sign Up </a></li>
+            <li><a href="{{ route('user.signin' )}}"> Sign In </a></li>
+            @endif
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
